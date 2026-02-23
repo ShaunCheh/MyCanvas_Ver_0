9b88d66fb5dd308388f3adc532617804a285c159
xiangjie yu <shaunyu@tokenbeat.ai>
2026-02-23 17:13:11 +0800

    Initial Commit

---
+++ MyCanvas_Ver_0/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MyCanvas_Ver_0
+//
+//  Created by Shaun on 2026/2/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
