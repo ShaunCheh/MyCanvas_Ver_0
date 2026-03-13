802ad173e288c617d64c46c7c63ba72788ef9495
xiangjie yu <shaunyu@tokenbeat.ai>
2026-03-13 11:13:24 +0800

    iOS hello world

--- MyCanvas_Ver_0/Platform/iOS/iOSViewController.swift
+++ MyCanvas_Ver_0/Platform/iOS/iOSViewController.swift
@@ -8,6 +8,32 @@
 import UIKit
 
 final class iOSViewController: UIViewController {
-    
+    private let helloLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = "Hello world"
+        label.font = .systemFont(ofSize: 32, weight: .semibold)
+        label.textColor = .label
+        label.textAlignment = .center
+        return label
+    }()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupViewHierarchy()
+        setupConstraints()
+    }
+
+    private func setupViewHierarchy() {
+        view.backgroundColor = .systemBackground
+        view.addSubview(helloLabel)
+    }
+
+    private func setupConstraints() {
+        NSLayoutConstraint.activate([
+            helloLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            helloLabel.centerYAnchor.constraint(equalTo: view.centerYAnchor)
+        ])
+    }
 }
 #endif
