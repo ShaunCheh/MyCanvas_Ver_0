64efcbe686fc59d76d6b70016b2b02652b4dcbe7
xiangjie yu <shaunyu@tokenbeat.ai>
2026-03-13 10:56:32 +0800

    20260313_105623 iOS initial commit

---
+++ MyCanvas_Ver_0/Platform/iOS/iOSAppDelegate.swift
@@ -0,0 +1,35 @@
+//
+//  iOSAppDelegate.swift
+//  MyCanvas_Ver_0
+//
+//  Created by Shaun on 2026/3/13.
+//
+
+#if os(iOS)
+import UIKit
+
+@main
+final class AppDelegate: UIResponder, UIApplicationDelegate {
+    var window: UIWindow?
+    
+    #if os(iOS)
+
+    #endif
+    func application(
+        _ application: UIApplication,
+        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]? = nil
+    ) -> Bool {
+        let window = UIWindow(frame: UIScreen.main.bounds)
+        window.rootViewController = iOSViewController()
+        // 需要设置背景色
+        // window.rootViewController = UINavigationController(rootViewController: iOSViewController())
+        // 不设置背景色，甚至不会触发-[UIApplication sendEvent:]
+        // window.backgroundColor = .systemBackground
+        window.backgroundColor = .white
+        window.makeKeyAndVisible()
+        self.window = window
+        return true
+    }
+
+}
+#endif
---
+++ MyCanvas_Ver_0/Platform/iOS/iOSViewController.swift
@@ -0,0 +1,13 @@
+//
+//  iOSViewController.swift
+//  MyCanvas_Ver_0
+//
+//  Created by Shaun on 2026/3/13.
+//
+#if canImport(UIKit) && !os(watchOS)
+import UIKit
+
+final class iOSViewController: UIViewController {
+    
+}
+#endif
